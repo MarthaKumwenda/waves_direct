7e539d0ab1fc042c98da9e97e0c36cbb6bfa2ff0
muka21 <nancymasamu@yahoo.com>
2017-09-28 16:07:38 +0200

    1) created a signup.html file 2) imported a usercreation form in wavesapp/urls.py 3) declared a signup form in wavesapp/views

--- wavesapp/views.py
+++ wavesapp/views.py
@@ -2,9 +2,26 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from wavesapp.forms import SignupForm
+from django.contrib.auth import login,authenticate
 
 # Create your views here
 def home(request):
     return render(request,'wavesapp/base.html',{})
 def waves_list(request):
     return render(request, 'wavesapp/waves_list.html',{})
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('base.html')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
